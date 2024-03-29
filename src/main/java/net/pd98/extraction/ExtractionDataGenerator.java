afc0c32bbb839a839e70477ad774f4fa322881ce
PuzzleDude98 <puzzledude98@outlook.com>
2024-03-29 08:52:07 -0700

    Initial commit

---
+++ src/main/java/net/pd98/extraction/ExtractionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pd98.extraction;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtractionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
