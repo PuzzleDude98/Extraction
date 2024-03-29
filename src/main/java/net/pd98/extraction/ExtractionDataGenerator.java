276f66e8f43444da6c93e515cdb5c7d30a53a441
PuzzleDude98 <puzzledude98@outlook.com>
2024-03-29 08:56:35 -0700

    Set up datagen

--- src/main/java/net/pd98/extraction/ExtractionDataGenerator.java
+++ src/main/java/net/pd98/extraction/ExtractionDataGenerator.java
@@ -2,10 +2,15 @@ package net.pd98.extraction;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.pd98.extraction.datagen.ModModelProvider;
+import net.pd98.extraction.datagen.ModRecipeProvider;
 
 public class ExtractionDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
