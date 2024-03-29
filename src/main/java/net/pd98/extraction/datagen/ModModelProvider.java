5f6a4353b42b028fab1190fe0bf76470588510c7
PuzzleDude98 <puzzledude98@outlook.com>
2024-03-29 09:16:31 -0700

    Extractor datagen

--- src/main/java/net/pd98/extraction/datagen/ModModelProvider.java
+++ src/main/java/net/pd98/extraction/datagen/ModModelProvider.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import net.pd98.extraction.item.ModItems;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -17,6 +19,6 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        itemModelGenerator.register(ModItems.EXTRACTOR, Models.GENERATED);
     }
 }
